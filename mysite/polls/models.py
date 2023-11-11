51810e7887616fbae76985b2e01fa296610b7d0d
zafarshodmonov <zafarshodmonov1996@gmail.com>
2023-11-11 18:27:11 +0500

    z

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,11 +1,23 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
 
 class Test(models.Model):
     title = models.CharField(max_length=255)
     # Add other fields for test information
+
 
 class Answer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
